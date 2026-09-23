3b52f631d74b8a5cfe8031b57a962d9fedae6ec1
juandav <e.juandav@gmail.com>
2026-09-23 04:32:47 -0500

    Match EAB_renderParticle and EVL_renderParticle (#89)

--- src/eab/eab.c
+++ src/eab/eab.c
@@ -562,7 +562,6 @@ void EAB_tickParticle(int32_t id)
 	}
 }
 
-#if 0
 void EAB_renderParticle(int32_t id)
 {
 	SVECTOR corners[4];
@@ -571,7 +570,6 @@ void EAB_renderParticle(int32_t id)
 	EabParticle *e;
 	LINE_F2 *prim;
 	int32_t size;
-	int32_t d;
 	int32_t i;
 	int32_t j;
 
@@ -593,11 +591,9 @@ void EAB_renderParticle(int32_t id)
 
 	prim = (LINE_F2 *)GsGetWorkBase();
 	for (i = 0; i < 4; i++) {
-		d = depth[i];
-		if ((d > 0x20) && (d < 0x1000)) {
+		if ((*(int32_t *)&depth[i] > 0x20) && (depth[i] < 0x1000)) {
 			j = (i + 1) % 4;
-			d = depth[j];
-			if ((d > 0x20) && (d < 0x1000)) {
+			if ((*(int32_t *)&depth[j] > 0x20) && (depth[j] < 0x1000)) {
 				SetLineF2(prim);
 				setRGB0(prim, lerp(e->r, 0, 0, 0x56, e->timer), lerp(e->g, 0, 0, 0x56, e->timer), lerp(e->b, 0, 0, 0x56, e->timer));
 				setXY2(prim, screen[i].vx, screen[i].vy, screen[j].vx, screen[j].vy);
@@ -608,9 +604,6 @@ void EAB_renderParticle(int32_t id)
 
 	GsSetWorkBase((PACKET *)prim);
 }
-#else
-INCLUDE_ASM("asm/eab/nonmatchings/eab", EAB_renderParticle);
-#endif
 
 void EAB_startBuildup(Entity *entity)
 {
